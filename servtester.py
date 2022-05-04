194cb53f951485c8c7e8907e4aac1c5a24a77586
jack <branchjohnchristopher@gmail.com>
2022-05-04 00:05:29 -0500

    servtest

--- servtester.py
+++ servtester.py
@@ -3,8 +3,10 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 factory = PiGPIOFactory()
 r = AngularServo(pin=16, min_pulse_width=0.0006, max_pulse_width=0.0023, pin_factory=factory )
-
-for i in range(-90, 91):
+r.angle = -52
+sleep(5)
+r.angle = 56
+""" for i in range(-90, 91):
     print(i)
     r.angle = i
-    sleep(1)
+    sleep(1) """
