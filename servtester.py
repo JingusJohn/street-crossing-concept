3e729d8732544fe509c8abc0fb3d3b809aea8dd8
jack <branchjohnchristopher@gmail.com>
2022-05-03 23:30:46 -0500

    fixed tester

--- servtester.py
+++ servtester.py
@@ -2,7 +2,7 @@ from gpiozero import AngularServo, Button
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 factory = PiGPIOFactory()
-r = Servo(pin=18, min_pulse_width=0.0006, max_pulse_width=0.0023, pin_factory=factory )
+r = AngularServo(pin=18, min_pulse_width=0.0006, max_pulse_width=0.0023, pin_factory=factory )
 for i in range(-90, 91):
     print(i)
     r.angle = i
