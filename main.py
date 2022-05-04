6499e8ef636cc95336a8fe0e21b379bddc3572fd
jack <branchjohnchristopher@gmail.com>
2022-05-03 22:53:06 -0500

    tweaking more

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class Gui(Frame):
         for i, srvc in enumerate(srvcs):
             Button(self.master, 
                 text=f"Call {srvc}", 
-                width=10,
+                width=23,
                 height=10,
                 bg=color[i], 
                 command=cmmds[i]).pack(side=LEFT)
